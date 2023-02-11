8ffe518fa2f9ce3f30c84930e61548303622a456
Eduardo Landeros <elanderos318@gmail.com>
2023-02-10 20:36:49 -0600

    split helper function to its own file

--- main.go
+++ main.go
@@ -58,15 +58,6 @@ func getFirstNames() []string {
 	return firstNames
 }
 
-func validateUserInput(
-	firstName string, lastName string, email string, userTickets uint,
-) (bool, bool, bool) {
-	var isValidName bool = len(firstName) >= 2 && len(lastName) >= 2
-	var isValidEmail bool = strings.Contains(email, "@")
-	isValidTicketNumber := userTickets > 0 && userTickets <= remainingTickets
-	return isValidName, isValidEmail, isValidTicketNumber
-}
-
 func getUserInputs() (string, string, string, uint) {
 	var firstName string
 	var lastName string
